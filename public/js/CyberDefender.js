82e590eca0e05b1075cccfb5eb2ff793d7d7434a
sai109 <lukebraithwaite7@gmail.com>
2018-10-22 17:37:04 +0100

    CyberDefender.js reformatted

--- public/js/CyberDefender.js
+++ public/js/CyberDefender.js
@@ -1,13 +1,16 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
+var segmentWidth = window.innerWidth / 2;
+var segmentHeight = window.innerHeight;
+
 var stage = new Konva.Stage({
 	container: 'container',
 	width: width,
 	height: height
 });
+
 var layer = new Konva.Layer();
-segmentWidth = window.innerWidth / 2;
-segmentHeight = window.innerHeight;
+var layer2 = new Konva.Layer();
 
 var lifePoint = new Konva.Text({
 	x: 50,
@@ -37,8 +40,6 @@ var segment2 = new Konva.Rect({
 	strokeWidth: 4
 });
 
-var layer2 = new Konva.Layer();
-
 cards.forEach(({ title, text }, num) => {
 	var card = new Konva.Rect({
 		x: segmentWidth / 2,
@@ -53,18 +54,16 @@ cards.forEach(({ title, text }, num) => {
 	});
 	layer2.add(card);
 	card.on('click', () => {
-		// Generate Modal in bootstrap
 		$('#card-text').text(text);
 		$('#card-title').text(title);
 		$('#myModal').modal();
 	});
 });
-// add the shape to the layer
+
 layer.add(segment1);
 layer.add(segment2);
 layer.add(lifePoint);
 
-// add the layer to the stage
 stage.add(layer);
 stage.add(layer2);
 
