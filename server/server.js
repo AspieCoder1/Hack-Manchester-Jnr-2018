e4cf01f2d0c70fb23d3468c3c198185ceaa5002c
sai109 <lukebraithwaite7@gmail.com>
2018-10-23 13:12:37 +0100

    heroku build set up

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
